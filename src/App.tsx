c16beedc3dd726822ea2a63491dcccb9985f8ac9
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2023-01-07 14:53:32 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Cart from './views/Cart/Cart';
+import Favourites from './views/Favourites/Favourites';
+import Homepage from './views/Homepage/Homepage';
+import NotFound from './views/NotFound/NotFound';
+import Order from './views/Order/Order';
+import Trips from './views/Trips/Trips';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path='' element={<Homepage/>}/>
+          <Route path='cart' element={<Cart/>}/>
+          <Route path='order' element={<Order/>}/>
+          <Route path='trips' element={<Trips/>}/>
+          <Route path='favourites' element={<Favourites/>}/>
+          <Route path='contact' element={<Homepage/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
