6a47b8015b8f613db34eebb5c8fd4474b16e6854
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2023-02-03 22:28:07 +0100

    Adjust contact by adding message state property

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../../common/Footer/Footer'
 import NavBar from '../../features/NavBar/NavBar'
 import './Contact.scss'
@@ -8,46 +8,62 @@ const Contact = () => {
         name: '',
         surname: '',
         email: '',
+        message: '',
     })
-    const [success, setSuccess] = useState(false);
-    const [disabled, setDisabled] = useState(true);
-    const [hint,setHint] = useState('')
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const [success, setSuccess] = useState(false)
+    const [disabled, setDisabled] = useState(true)
+    const [hint, setHint] = useState('')
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const { name, surname, email, message } = formData
+
+    useEffect(() => {
+        if (!message || !surname || !email || !regex.test(email)) {
+            setDisabled(true)
+        } else {
+            setDisabled(false)
+        }
+    }, [name, surname, email, message])
 
     const handleHint = (targetInput) => {
-        if(!targetInput.value) {
+        const targetInputValue = targetInput.value
+        if (!targetInputValue) {
             setHint(`You ${targetInput.name} field must not be empty`)
-        }
-        if(targetInput.name==="email" && !regex.test(targetInput.value)) {
-            setHint(`Your email is not valid`)
-        }
-        if(regex.test(targetInput.value)) {
+        } else {
             setHint('')
+        }
+
+        if (targetInput.name === 'email') {
+            const testEmail = regex.test(targetInputValue)
+            if (!testEmail) {
+                setHint(`Your email is not valid`)
+            }
+            if (testEmail) {
+                setHint('')
+            }
         }
     }
 
     const onChangeHandler = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value })
         handleHint(e.target)
-        if(name && surname && email && regex.test(email)) {
-            setDisabled(false)
-        }
     }
+
     const onSubmitHandler = (e) => {
         e.preventDefault()
         console.log(formData)
-        setSuccess(true);
+        setSuccess(true)
         setFormData({
             name: '',
             surname: '',
             email: '',
+            message: '',
         })
         setTimeout(() => {
             setSuccess(false)
             setDisabled(true)
-        }, 3000);
+            setHint('')
+        }, 3000)
     }
-    const { name, surname, email } = formData
     return (
         <div>
             <div className="contact_nav_container">
@@ -87,9 +103,29 @@ const Contact = () => {
                                 placeholder="Email"
                             />
                         </div>
-                        <input className={disabled && "disabled"} type="submit" value="Submit" disabled={disabled} />
-                        {success && <h5 className='form_message'>Your message has been successfully sent!</h5>}
-                        <h5 className='form_message'>{hint}</h5>
+                        <div className="form_group">
+                            <label htmlFor="message"></label>
+                            <textarea
+                                name="message"
+                                placeholder="Please type your message"
+                                value={message}
+                                onChange={onChangeHandler}
+                                rows={4}
+                                cols={50}
+                            ></textarea>
+                        </div>
+                        <input
+                            className={disabled ? 'disabled' : ''}
+                            type="submit"
+                            value="Submit"
+                            disabled={disabled}
+                        />
+                        {success && (
+                            <h5 className="form_message">
+                                Your message has been successfully sent!
+                            </h5>
+                        )}
+                        <h5 className="form_message">{hint}</h5>
                     </form>
                 </div>
             </div>
