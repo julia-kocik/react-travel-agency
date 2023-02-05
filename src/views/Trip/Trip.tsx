45ef5c88e64e26a383b14cdb8da11cbab00d2ccf
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2023-02-05 13:51:26 +0100

    Add Trip component

--- src/views/Trip/Trip.tsx
+++ src/views/Trip/Trip.tsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './Trip.scss'
 import data from '../../data.json'
-import Navlinks from '../../common/Navlinks/Navlinks'
-import Navigation from '../../features/NavBar/NavItems/Navigation'
+import { Link } from 'react-router-dom'
 import Footer from '../../common/Footer/Footer'
 import NavBar from '../../features/NavBar/NavBar'
+import Button from '../../common/Button/Button'
 
 interface TripInterface {
     id: number
@@ -31,19 +31,46 @@ const Trip = () => {
     const { id } = useParams()
     const trips = data
     const [trip, setTrip] = useState(TripInitialObject)
+
+    const { destination, name, description, price, places, status } = trip
+
     useEffect(() => {
         const selected = trips.find((item) => item.id === +id)
         setTrip(selected)
     }, [])
+
     return (
         <div>
-             <div className="trip_nav_container">
+            <div className="trip_nav_container">
                 <NavBar />
             </div>
-            <div>
-                {trip?.id}
-                {trip.name}
-                {trip.description}
+            <div className="trip_content_container">
+                <div
+                    className="trip_photo"
+                    style={{
+                        backgroundImage: `url("/images/${destination}.jpg")`,
+                    }}
+                ></div>
+                <div className="trip_content">
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                    <div className="trip_details">
+                        <span>
+                            <b>Price:</b> {price}$
+                        </span>
+                        <span>
+                            <b>Places:</b> {places}
+                        </span>
+                        <span>
+                            <b>Status:</b> {status}
+                        </span>
+                    </div>
+                    <div className="trip_btn_container">
+                        <Link className="nav_links header_btn" to="/contact">
+                            <Button title="Order" color="dark" />
+                        </Link>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
