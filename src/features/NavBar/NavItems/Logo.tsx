24521a5b7b5137943d67560ce7e9e0aef7eafb6e
juliakocik <julia.kocik@gmail.com>
2023-09-25 17:03:58 +0200

    Add title change

--- src/features/NavBar/NavItems/Logo.tsx
+++ src/features/NavBar/NavItems/Logo.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <div className='logo_container'>
-      <span className='logo'> <Link className='nav_links' to="/">Travel<br />Life</Link></span>
+      <span className='logo'> <Link className='nav_links' to="/">My<br />Journey</Link></span>
     </div>
   )
 }
